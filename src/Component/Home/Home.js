cda2c796dfb61e81ac95b33698862b9f107c187d
mestrpra <prashant.mestry@dspim.com>
2022-12-30 00:37:20 +0530

    click

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -221,7 +221,7 @@ class Home extends React.Component {
                         this.setState({
                             openDrawer: true
                         })
-                    }}>Notification Click here</button>
+                    }}>Notification Click</button>
                 </div>
 
                 <PageSection className='pad-10'>
