2216a52461d9bef4d9c490eca8ed75c1f3f9bc23
mestrpra <prashant.mestry@dspim.com>
2023-01-08 11:11:11 +0530

    login with Auth0 , user info display

--- src/Component/Common/Header/HeaderInformation.js
+++ src/Component/Common/Header/HeaderInformation.js
@@ -1,14 +1,20 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Image } from 'antd';
 import React from 'react';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButto';
 
 const HeaderInformation = (props) => {
+    const { user, isAuthenticated } = useAuth0();
     return (
         <div className='flex justify-between align-center pad-l-10 pad-r-10'>
-            <div>One
-
-        { process.env.REACT_APP_JARVIS_FASTAPI_URL}
-            </div>
+            {
+                user &&
+                <div className='flex'>
+                    <div><Image src={user.picture} /></div>
+                    <div>{user.name}</div>
+                </div>
+            }
             <div>
                 <LoginButton />
                 <LogoutButton />
